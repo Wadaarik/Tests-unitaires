538bb8cc3dc0d1c5a39a010ad32ef5aa6a37bb77
Wadaarik <nicolasboudier12@gmail.com>
2021-04-26 14:16:43 +0200

    first style edit

--- src/esport.localhost/index.php
+++ src/esport.localhost/index.php
@@ -1,2 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/style/style.css">
+    <title>Dat project</title>
+</head>
+<body>
 <?php
-echo 'hello';
+//echo 'hello';
+?>
+</body>
+</html>
